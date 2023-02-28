96e7841944cccb1c70842959bb7aade10bdddcee
LuBlackni <125086327+LuBlackni@users.noreply.github.com>
2023-02-28 14:11:49 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,5 @@
+document.getElementById("boton").addEventListener("click", function () {
+    console.log("Contacto");
+    document.getElementById("demo").innerHTML = "Cel +543416068222 martinezluciana@gmail.com";
+
+});
