dc4cebf5ed86639605de5db06d9526c00b128b44
LuBlackni <125086327+LuBlackni@users.noreply.github.com>
2023-02-28 20:14:53 -0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("boton").addEventListener("click", function () {
-    console.log("Contacto");
-    document.getElementById("demo").innerHTML = "Cel +543416068222 martinezluciana@gmail.com";
-
-});
